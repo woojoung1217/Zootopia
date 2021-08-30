ec7591a6a03aeff914643f6346289a32208d804f
Chae hee chan <eqholic1125@gmail.com>
2021-08-30 23:52:52 +0900

    setting

--- Zootopia/Zootopia/urls.py
+++ Zootopia/Zootopia/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Animal import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name="main"),
 ]
