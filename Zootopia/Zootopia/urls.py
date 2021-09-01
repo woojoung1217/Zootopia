00b7846d90b1454b24ebac836d78c23d7f5366e5
Chae hee chan <eqholic1125@gmail.com>
2021-09-01 19:34:53 +0900

    [BE]구현: views.py 연결(희찬)

--- Zootopia/Zootopia/urls.py
+++ Zootopia/Zootopia/urls.py
@@ -5,7 +5,13 @@ from animal import views as animalViews
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls.conf import include
+from account import views as A
+from animal import views as B
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', animalViews.main, name="main"),
+    path('', B.main, name="main"),
+    path('account/', include('account.urls')),
+    path('animal/', include('animal.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
