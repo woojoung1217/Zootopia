d3bb807a9214f8ec3f77bc661d32c9c13c1065cf
woojoung <ywj981217@naver.com>
2021-09-06 22:41:28 +0900

    [FE]:구현main

--- Zootopia/Animal/urls.py
+++ Zootopia/Animal/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('',  views.main, name="main"),
     path('search/', views.search, name="search"),
     path('result/', views.result, name="result"),
+    path('main/', views.main, name="main"),
 ]
