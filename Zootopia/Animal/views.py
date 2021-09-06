08a820282dd303a353f1ce3a4221f5faec1645d0
Chae hee chan <eqholic1125@gmail.com>
2021-09-06 18:01:05 +0900

    [BE] 구현: 검색 페이지 최근글 read 기능 구현(희찬)

--- Zootopia/Animal/views.py
+++ Zootopia/Animal/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from animal.models import Post
+from .models import *
 
 def main (request):
   return render (request, 'main.html')  
 
 def search (request):
-  return render (request, 'search.html')
+  posts = Post.objects.all()
+  return render (request, 'search.html', {"posts":posts})
 
 def result(request):
-  return render (request, 'result.html')
+  posts = Post.objects.all()
+  return render (request, 'result.html', {"posts":posts})
