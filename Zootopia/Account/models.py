b48d5d56ec7396438bf40a58396abb752ecc245d
Lee Ji Heok <olzlgur@naver.com>
2021-09-07 13:53:35 +0900

    [BE] 구현 : 유저모델 생성 (지혁)

--- Zootopia/Account/models.py
+++ Zootopia/Account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser) :
+    nickname = models.CharField(max_length=30, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='account/', blank=True, null=True)
+    introduce = models.TextField(max_length = 100, blank=True, null=True)
