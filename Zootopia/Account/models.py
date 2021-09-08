00f63a9e4d95fb433d26774e01e8f2fb24b05c01
Lee Ji Heok <olzlgur@naver.com>
2021-09-08 15:58:33 +0900

    [FE] 수정 : profile 페이지 디자인 수정 (지혁)

--- Zootopia/Account/models.py
+++ Zootopia/Account/models.py
@@ -3,6 +3,6 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser) :
-    nickname = models.CharField(max_length=30, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='account/', blank=True, null=True)
-    introduce = models.TextField(max_length = 100, blank=True, null=True)
+    nickname = models.CharField(max_length=30, blank=True, null=True)     # 유저 닉네임 모델
+    profile_picture = models.ImageField(upload_to='account/', blank=True, null=True)   # 유저 프로필 사진 모델
+    introduce = models.TextField(max_length = 100, blank=True, null=True)  # 유저 자기소개 모델
