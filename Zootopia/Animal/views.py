55869d833ff6115cd1315c371ef550c1a746a7a8
Lee Ji Heok <olzlgur@naver.com>
2021-09-15 01:36:54 +0900

    [BE] : 게시글 작성 구현(지혁)

--- Zootopia/Animal/views.py
+++ Zootopia/Animal/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from animal.models import Post
 from .models import *
+from account.models import User
 
 def main (request):
   return render (request, 'main.html')  
@@ -9,7 +10,23 @@ def detail(request):
   return render (request, 'detail.html')  
 
 def upload (request):
-  return render (request, 'upload.html')  
+   if request.method == "POST":  # method가 Post일 때 글 작성
+      post_blog = Post()
+      post_blog.name= request.POST.get('name')
+      post_blog.time = timezone.datetime.now() 
+      post_blog.image = request.FILES.get('image')
+      post_blog.body = request.POST.get('body')
+      post_blog.hash_tag = request.POST.get('hashtag')
+      post_blog.species = request.POST.get('species')
+
+      user_id = request.user.id
+      user = User.objects.get(id = user_id)
+      post_blog.author = user
+      post_blog.save()
+      return redirect('/')
+
+   else:
+      return render (request, 'upload.html')  
 
 def search (request):
   posts = Post.objects.all()[Post.objects.count()-5::-1]
@@ -21,3 +38,4 @@ def result(request):
 
 def profile(request):
   return render(request,'profile.html')
+
