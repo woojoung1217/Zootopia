f0d8bf36b91463387cf82431e7c1f10044b7ad31
Lee Ji Heok <olzlgur@naver.com>
2021-09-17 16:40:32 +0900

    [BE] 수정 : 모델 수정(지혁)

--- Zootopia/Animal/models.py
+++ Zootopia/Animal/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+  
   # 동물 이름
   name = models.CharField(max_length=20)
   # 동물 종
@@ -13,13 +14,14 @@ class Post(models.Model):
   # 사진
   image = models.ImageField(upload_to='post/', blank=True, null=True)
   # 작성자
-  time =  models.DateTimeField(auto_now_add=True)
-  # 작성 시간
-  latitude = models.FloatField(default=0.0) 
-  #위도
-  longitude = models.FloatField(default=0.0) 
-  #경도
 
+  # 작성 시간
+  time =  models.DateTimeField(auto_now_add=True)
+  #위도
+  latitude = models.FloatField(default=0.0) 
+  #경도
+  longitude = models.FloatField(default=0.0) 
+  
   def __str__(self):
     return self.name
 
